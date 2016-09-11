c366e91fc945cc8510db18cf2a06fe8f10e20704
hongming li <hongmingli1988@gmail.com>
2016-09-10 17:32:52 -0700

    add tmid for teams

--- webconfig_template.js
+++ webconfig_template.js
@@ -1,8 +1,15 @@
 
+// MongoDB Config
 module.exports.mongodb = {
 	username : 'test',
 	password : 'test',
 	database : 'test',
 	host : 'localhost',
 	port: 27017
+};
+
+// TM Config
+module.exports.tm = {
+  consumerKey: 'xxxxxxxx',
+  consumerSecret: 'yyyyyyy'
 };
